27623d739607ac41819972805df92874fe2a170f
RuShHaar <stefan.ockert@gmx.net>
2021-05-18 13:38:35 +0200

    Names-Klasse hinzugefügt

---
+++ src/Rollenspielsetup/Names.java
@@ -0,0 +1,46 @@
+package Rollenspielsetup;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+public class Names {
+
+    public static String getRandomName() {
+
+        String fileName = "rollenspiel_namen.csv";
+        File file = new File(fileName);
+        List<String> result = new ArrayList<>();
+
+        try {
+            // -Datei mit Scanner auslesen
+            Scanner inputStream = new Scanner(file);
+
+            // hashNext() loops line-by-line
+            while (inputStream.hasNext()) {
+                //read single line, put in string
+                String[] data = inputStream.next().split(" ");
+
+                //Array in Arraylist parsen
+                Collections.addAll(result, data);
+
+            }
+
+            // Scanner schließen
+            inputStream.close();
+
+        } catch (FileNotFoundException e) {
+
+            e.printStackTrace();
+        }
+        // Zufälligene Namen auswählen
+        Random randomNumber = new Random();
+        int selector = randomNumber.nextInt(result.size());
+
+        return result.get(selector);
+
+
+    }
+}
